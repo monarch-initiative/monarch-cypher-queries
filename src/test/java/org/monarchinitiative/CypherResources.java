0caa6d5c0e3a341879df1b7398ee243a0e5ff6aa
Ben Booth <benwbooth@gmail.com>
2017-06-01 10:14:46 -0700

    [#12] Fix unit tests for latest scigraph compatibility

--- src/test/java/org/monarchinitiative/CypherResources.java
+++ src/test/java/org/monarchinitiative/CypherResources.java
@@ -8,20 +8,21 @@ import javax.validation.Valid;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import io.scigraph.services.swagger.beans.resource.Apis;
+// TODO: update to use Swagger 2.0 data models
+//import io.scigraph.services.swagger.beans.resource.Apis;
 
 public class CypherResources {
-  @Valid
-  @JsonProperty(required = false)
-  private List<Apis> cypherResources = Collections.emptyList();
+  //@Valid
+  //@JsonProperty(required = false)
+  //private List<Apis> cypherResources = Collections.emptyList();
 
   public Optional<String> getCypherResource(String path) {
     Optional<String> hit = Optional.empty();
-    for (Apis api : cypherResources) {
-      if (api.getPath().equals(path)) {
-        hit = Optional.of(api.getQuery());
-      }
-    }
+    //for (Apis api : cypherResources) {
+    //  if (api.getPath().equals(path)) {
+    //    hit = Optional.of(api.getQuery());
+    //  }
+    //}
     return hit;
   }
 }
