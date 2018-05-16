14cba170d75d9d3191fa77c486daa81b01308947
kshefchek <kshefchek@gmail.com>
2018-05-15 23:56:38 -0700

    updates to kg transform

--- src/main/cypher/kg-transform/make-cypher.sh
+++ src/main/cypher/kg-transform/make-cypher.sh
@@ -5,5 +5,7 @@ cat add-node-properties.cql dereification.cql > transform-graph.cql
 # Hack until https://github.com/neo4j-contrib/neo4j-apoc-procedures/issues/163
 sed -e '/AGGREGATE_EVIDENCE_SOURCE_PROPS/ r aggregate-evidence.txt' -e s/AGGREGATE_EVIDENCE_SOURCE_PROPS//  gene-disease.cql >> transform-graph.cql
 sed -e '/AGGREGATE_EVIDENCE_SOURCE_PROPS/ r aggregate-evidence.txt' -e s/AGGREGATE_EVIDENCE_SOURCE_PROPS//  gene-phenotype.cql >> transform-graph.cql
+sed -e '/AGGREGATE_EVIDENCE_SOURCE_PROPS/ r aggregate-evidence.txt' -e s/AGGREGATE_EVIDENCE_SOURCE_PROPS//  owl-axioms.cql >> transform-graph.cql
+sed -e '/AGGREGATE_EVIDENCE_SOURCE_PROPS/ r aggregate-evidence.txt' -e s/AGGREGATE_EVIDENCE_SOURCE_PROPS//  gene-gene.cql >> transform-graph.cql
 
 cat remove-edges.cql >> transform-graph.cql
